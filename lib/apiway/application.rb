61db07de6e3ec8c23dee85198a8818b07879e598
4urbanoff <4urbanoff@gmail.com>
2015-09-23 10:38:52 +0300

    fix load order

--- lib/apiway/application.rb
+++ lib/apiway/application.rb
@@ -7,23 +7,7 @@ module Apiway
     set static:            true
     set apiway_log:        true
     set active_record_log: true
-    set log_level:         :debug
     set database_file:     File.join( root, 'config/database.yml' )
-
-
-    register Sinatra::ActiveRecordExtension
-
-
-    configure :development do
-      register Sinatra::Reloader
-      also_reload File.join( root, '**/*.rb' )
-      # also_reload File.join( Apiway.path, '**/*.rb' )
-    end
-
-
-    get '*' do
-      request.websocket? ? request.websocket{ |ws| Apiway::Client.new ws } : pass
-    end
 
 
     %W(
@@ -39,8 +23,23 @@ module Apiway
     .flatten.uniq.each{ |path| require path }
 
 
+    register Sinatra::ActiveRecordExtension
+
+
     LoggerBase::apiway_log_level       apiway_log
     LoggerBase::activerecord_log_level activerecord_log
+
+
+    configure :development do
+      register Sinatra::Reloader
+      also_reload File.join( root, '**/*.rb' )
+      # also_reload File.join( Apiway.path, '**/*.rb' )
+    end
+
+
+    get '*' do
+      request.websocket? ? request.websocket{ |ws| Apiway::Client.new ws } : pass
+    end
 
 
     def self.tasks
